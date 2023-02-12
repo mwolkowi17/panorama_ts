94a6f12aa2d352463da8d0147e45be5973634070
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-02-12 11:57:07 +0100

    new class added

--- src/client/panorama1.ts
+++ src/client/panorama1.ts
@@ -6,16 +6,6 @@ import { panorama2,navicon2,navicon3 } from './panorama2';
 import { scene } from './client';
 import { panorama_form } from './panorama_create';
 
-//panorama image
-
-// const geometry = new THREE.SphereGeometry(500, 60, 40);
-// // invert the geometry on the x-axis so that all of the faces point inward
-// geometry.scale(- 1, 1, 1);
-
-// const texture = new THREE.TextureLoader().load('./index.png');
-// const material = new THREE.MeshBasicMaterial({ map: texture });
-
-//export const panorama1 = new THREE.Mesh(geometry, material);
 export const panorama1 = new panorama_form('./index.png').main;
 
 export const navicon1 = new create_icon(-12, -18, -30,'nav1');
